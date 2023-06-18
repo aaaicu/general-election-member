92ee85efbadbc04dc47af4deb39ff77d3ce9bd77
aaaicu <aaaaicu@gmail.com>
2023-06-18 19:27:35 +0900

    test: 환경롤백

--- src/main/kotlin/com/parsrich/generalelectionmember/ui/auth/controller/AuthController.kt
+++ src/main/kotlin/com/parsrich/generalelectionmember/ui/auth/controller/AuthController.kt
@@ -8,43 +8,27 @@ import com.parsrich.generalelectionmember.ui.auth.dto.KakaoLoginResponseDTO
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import java.util.*
 
 @RequestMapping(EndPoint.AUTH)
 @RestController
 class AuthController(val authServiceFactory: AuthServiceFactory) {
 
-//    @GetMapping("/{authSocialType}")
-//    fun authorize(@PathVariable authSocialType: Optional<AuthSocialType>): ResponseEntity<String> {
-//        val service = authServiceFactory.getService(authSocialType.get())
-//        val authorize = service.authorize(String::class.java)
-//        println("authorize = $authorize")
-//
-//        val responseHeaders = HttpHeaders()
-//        responseHeaders.add("Content-type", "text/html; charset=UTF-8")
-//
-//        return ResponseEntity.ok().headers(responseHeaders).body(authorize)
-//    }
+    @GetMapping("/{authSocialType}")
+    fun authorize(@PathVariable authSocialType: Optional<AuthSocialType>): ResponseEntity<String> {
+        val service = authServiceFactory.getService(authSocialType.get())
+        val authorize = service.authorize(String::class.java)
+        println("authorize = $authorize")
 
-//    @GetMapping("/{authSocialType}/callback")
-//    fun callback(@PathVariable authSocialType: Optional<AuthSocialType>, @RequestParam code: String): ResponseEntity<KakaoLoginResponseDTO> {
-//        logger.info("정상 로그: $code")
-//
-//        val service = authServiceFactory.getService(authSocialType.get())
-//        service.callback()
-//
-//        return ResponseEntity.ok().build()
-//    }
+        val responseHeaders = HttpHeaders()
+        responseHeaders.add("Content-type", "text/html; charset=UTF-8")
 
+        return ResponseEntity.ok().headers(responseHeaders).body(authorize)
+    }
 
     @GetMapping("/{authSocialType}/callback")
-    fun callback(
-        @PathVariable authSocialType: Optional<AuthSocialType>,
-        redirectAttributes: RedirectAttributes
-    ): ResponseEntity<KakaoLoginResponseDTO> {
-
-        logger.info("정상 로그: $redirectAttributes")
+    fun callback(@PathVariable authSocialType: Optional<AuthSocialType>, @RequestParam code: String): ResponseEntity<KakaoLoginResponseDTO> {
+        logger.info("정상 로그: $code")
 
         val service = authServiceFactory.getService(authSocialType.get())
         service.callback()
